fc5bf12856dcd51db3e30b42a6580b4e64b938b3
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-02-13 23:32:50 -0600

    add coffee page

---
+++ src/app/pages/coffee/coffee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'coffee',
+  templateUrl: './coffee.component.html',
+  styleUrls: ['./coffee.component.scss']
+})
+
+export class CoffeeComponent {
+
+}
