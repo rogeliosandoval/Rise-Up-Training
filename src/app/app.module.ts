5006d247b8dcfd232ec1dce2bd095ff0a4c2a030
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-02-14 20:48:48 -0600

    update the doom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BowlsComponent } from './pages/bowls/bowls.component'
+import { SmoothiesComponent } from './pages/smoothies/smoothies.component';
+import { CoffeeComponent } from './pages/coffee/coffee.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'bowls', component: BowlsComponent },
+  { path: 'smoothies', component: SmoothiesComponent },
+  { path: 'coffee', component: CoffeeComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BowlsComponent,
+    SmoothiesComponent,
+    CoffeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })
   ],
   providers: [],
   bootstrap: [AppComponent]
